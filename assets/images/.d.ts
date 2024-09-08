4e919d02519b577715529d8b9d8d917487182c76
suresh5189 <suru242000@gmail.com>
2024-09-08 23:30:11 +0530

    File Updated

--- assets/images/.d.ts
+++ assets/images/.d.ts
@@ -1,14 +1,14 @@
-declare module '*.png' {
-    const value: number;
-    export = value;
-  }
-  
-  declare module '*.jpg' {
-    const value: number;
-    export = value;
-  }
-  
-  declare module '*.webp' {
-    const value: number;
-    export = value;
-  }
+declare module "*.png" {
+  const value: number;
+  export = value;
+}
+
+declare module "*.jpg" {
+  const value: number;
+  export = value;
+}
+
+declare module "*.webp" {
+  const value: number;
+  export = value;
+}
